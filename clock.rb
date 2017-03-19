68fd4d03e3ef2f7f7a337f2b5a8e4a29ea4adc1b
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-03-19 05:47:16 +0100

    Replacing clock with last fetch time

--- clock.rb
+++ clock.rb
@@ -1,35 +1,34 @@
 # clock.rb:  Displays the current time
 class Clock < Hyperloop::Component
-  param format: '%a, %e %b %Y %H:%M'
+  param format: '%a, %e %b %Y %H:%M:%S'
   before_mount do
-    mutate.time Time.now.strftime(params.format)
     mutate.running true
     get_last_bitcoin_price
-    every(1) { mutate.time Time.now.strftime(params.format) if state.running}
     every(10) { get_last_bitcoin_price if state.running }
   end
 
   def get_last_bitcoin_price
     HTTP.get("https://blockchain.info/ticker").then{ |response|
+      mutate.time Time.now.strftime(params.format)
       mutate.last response.json["USD"]["last"]
     }
   end
 
-  def toggle_time
+  def toggle_fetching
     mutate.running !state.running
   end
 
   render do
-    div(class: "container") do
-      H1 {"clock"}
-      H2 {state.time}
-      if state.running
-        button.btn_danger.btn_xs {'STOP'}
-      else
-        button.btn_success.btn_xs {'START'}
-      end.on(:click) { toggle_time }
+    DIV.container do
+      H1 {"Bitcoin"}
       div.jumbotron do
         h1 { "BTC/USD #{state.last}" }
+        p { "Fetched at #{state.time}" }
+        if state.running
+          button.btn_danger.btn_xs {'STOP'}
+        else
+          button.btn_success.btn_xs {'START'}
+        end.on(:click) { toggle_fetching }
         button.btn_default.btn_xs {'REFRESH'}.on(:click){ get_last_bitcoin_price }
       end
     end
