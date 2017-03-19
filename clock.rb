1e9862c072b462b91f402193c90b187992ddd50a
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-03-19 05:51:14 +0100

    Renaming state running to auto_refresh

--- clock.rb
+++ clock.rb
@@ -2,9 +2,9 @@
 class Clock < Hyperloop::Component
   param format: '%a, %e %b %Y %H:%M:%S'
   before_mount do
-    mutate.running true
+    mutate.auto_refresh true
     get_last_bitcoin_price
-    every(10) { get_last_bitcoin_price if state.running }
+    every(10) { get_last_bitcoin_price if state.auto_refresh }
   end
 
   def get_last_bitcoin_price
@@ -14,21 +14,17 @@ class Clock < Hyperloop::Component
     }
   end
 
-  def toggle_fetching
-    mutate.running !state.running
-  end
-
   render do
     DIV.container do
       H1 {"Bitcoin"}
       div.jumbotron do
         h1 { "BTC/USD #{state.last}" }
         p { "Fetched at #{state.time}" }
-        if state.running
+        if state.auto_refresh
           button.btn_danger.btn_xs {'STOP'}
         else
           button.btn_success.btn_xs {'START'}
-        end.on(:click) { toggle_fetching }
+        end.on(:click) { mutate.auto_refresh !state.auto_refresh }
         button.btn_default.btn_xs {'REFRESH'}.on(:click){ get_last_bitcoin_price }
       end
     end
