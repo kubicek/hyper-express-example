08ae9d6396623331953278731fee95ab270e1dcd
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-03-19 04:38:55 +0100

    Adding stop button

--- clock.rb
+++ clock.rb
@@ -3,13 +3,19 @@ class Clock < Hyperloop::Component
   param format: '%a, %e %b %Y %H:%M'
   before_mount do
     mutate.time Time.now.strftime(params.format)
-    every(1) { mutate.time Time.now.strftime(params.format) }
+    every(1) { mutate.time Time.now.strftime(params.format) unless state.status=="stopped"}
+  end
+
+  def stop_time
+    mutate.status "stopped"
+    puts state.status
   end
 
   render do
     div(class: "container") do
       H1 {"clock"}
       H2 {state.time}
+      button.btn_danger.btn_xs {'STOP'}.on(:click) { stop_time }
     end
   end
 end
